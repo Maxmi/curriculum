afd08a3d0119ba44c703724ab505a2e1be243313
Jared Grippe <jared@deadlyicon.com>
2017-07-12 14:42:30 -0700

    WIP

---
+++ database/migrations/20170712124746_migrate-skills.js
@@ -0,0 +1,30 @@
+const CHANGES = {
+  "Can use JavaScript's `Array#sort`": "Can use `Array#sort` in JavaScript",
+  "Can iterate through an Array": "Can iterate through an Array in JavaScript",
+  'Can describe what "state" is': "Can describe the general programming term `state`",
+  "Can explain what encryption is, and why it is important when dealing with sensitive information(eg: passwords)": [
+    "Can describe what encryption is",
+    "Can describe why encryption is important when dealing with sensitive information like passwords",
+  ]
+  "Can read and write to files using `fs.readFileSync`": "Can read and write to files using `fs.readFileSync` in Node"
+  "Can explain what the MVC(Model View Controller) pattern is": "Can describe the general programming pattern `MVC` (Model View Controller)",
+  "Can style typographic elements (headers, paragraphs, etc.)": "Can style typographic elements (headers, paragraphs, etc.) in HTML & CSS",
+  "Can position elements with `position: absolute` in HTML & CSS": "Can position elements with `position: absolute` in CSS",
+  "Can position elements with `position: relative` in HTML & CSS": "Can position elements with `position: relative` in CSS",
+  "Can hide elements using `display` in HTML & CSS": "Can hide elements using `display` in CSS",
+  "Can convert a mockup into an HTML page with proper styling": "Can convert a mockup into an HTML & CSS page with proper styling",
+  'Can describe what "Defensive Programming" means': "Can describe the general programming term `Defensive Programming`",
+  "Can give two examples of \"Defensive Programming\"": "Can give two examples of \"Defensive Programming\" in JavaScript",
+
+
+-- Can give two examples of "Defensive Programming"
++- Can give two examples of "Defensive Programming" in JavaScript
+}
+
+exports.up = function(knex, Promise) {
+
+};
+
+exports.down = function(knex, Promise) {
+
+};
